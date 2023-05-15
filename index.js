d9858fbcf2da301c108d66086dfd00a41c232bcd
BondT1 <tombond248@gmail.com>
2023-05-15 16:34:46 +0100

    fetch weather api started

--- index.js
+++ index.js
@@ -11,3 +11,18 @@ var cityEl = $('#city');
 var fiveForecastEl = $('#five-forecast');
 var weatherIcon = 'http://openweathermap.org/img/wn/';
 var citySearch = [];
+
+function fetchWeather(city) {
+    var coordinates = `${openWeatherUrl}${city}&appid=${ApiKey}`
+    fetch (coordinates) 
+        .then (function (coordinateResult) {
+            if (coordinateResult.ok) {
+                coordinateResult.json().then(function (data) {
+                    var lon = data.coord.lon;
+                    var lat = data.coord.lat;
+                    var apiFetch = forecastUrl + lat + '&lon=' + lon + '&appid=' + ApiKey + '&units=metric';
+
+                    fetch(apiFetch)
+                        .then(function (weatherResult) {
+                            if (weatherResult.ok) {
+                                weatherResult.json().then(function (weatherInfo) {
